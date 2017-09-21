a1ff8689cfc6634cf633b53f5b0350f4d91a675b
席萍萍 Xi Pingping <cirpxpp@163.com>
2017-09-21 15:10:49 +0800

    update header file

--- TestGSPromise/TestGSPromise/GSPromise.h
+++ TestGSPromise/TestGSPromise/GSPromise.h
@@ -10,10 +10,10 @@
 
 /**
  what a promise will execute
-
- @param then should call this block after promise done
+ 
+ @param ok should call this block after promise done
  */
-typedef void(^GSPromiseHandler)(dispatch_block_t then);
+typedef void(^GSPromiseHandler)(dispatch_block_t ok);
 
 @protocol GSPromisable <NSObject>
 /**
@@ -33,28 +33,32 @@ typedef void(^GSPromiseHandler)(dispatch_block_t then);
 
 // Convenience initializers
 - (instancetype)initWithHandler:(GSPromiseHandler)handler;
++ (instancetype)promise;
 + (instancetype)promiseWithHandler:(GSPromiseHandler)handler;
 
 @end
 
 /**
  A promise manager who managers a array of `promisable` objects
- a promise mananger can will be captured in promise's handler block within handler's `then` block parameter
+ a promise mananger can will be captured in promise's handler block within handler's `ok` block parameter
  */
 @interface GSPromiseManager : NSObject
 
+
 /**
  add a promise, it will start as soon as possible
-
+ 
  @param promise a promise confirms to `promisable` protocol
  */
 - (void)addPromise:(id<GSPromisable>)promise;
 
 // Convenience initilizers
-- (instancetype)initWithPromise:(id <GSPromisable>)promise;
-+ (instancetype)manangerWithPromise:(id <GSPromisable>)promise;
-- (instancetype)initWithPromises:(NSArray <id <GSPromisable>> *)promises;
-+ (instancetype)manangerWithPromises:(NSArray <id <GSPromisable>> *)promises;
+- (instancetype)initWithPromise:(id<GSPromisable>)promise;
+- (instancetype)initWithPromises:(NSArray<id<GSPromisable>> *)promises;
+
++ (instancetype)manager;
++ (instancetype)manangerWithPromise:(id<GSPromisable>)promise;
++ (instancetype)manangerWithPromises:(NSArray<id<GSPromisable>> *)promises;
 
 @end
 
